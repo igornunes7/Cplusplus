02a7816595a2a00832a352351239a7a68d231895
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2024-09-18 19:37:38 -0400

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -6,23 +6,11 @@ using namespace std;
 template <typename T>
 
 class Vetor {
-    private:
-
+private:
     vector<T> v;
-
-    public:
-
-    void inserirVector (T data) {
+public:
+    void inserir(T data) {
         v.push_back(data);
-        for (int i = 0; i < v.size() - 1; i++) {
-            for (int j = 0; j < v.size() - 1 - i; j++) {
-                if (v[j] > v[j + 1]) {
-                    T temp = v[j];
-                    v[j] = v[j + 1];
-                    v[j + 1] = temp;
-                }
-            }
-        }
     }
 
     void inserirMiddle (T data) {
@@ -34,7 +22,7 @@ class Vetor {
 
         v.clear();
 
-        for (int i = 0; i < v_copy.size(); i++) {
+        for (int i = 0; i < v_copy.size() ; i++) {
             if (i == mid) {
                 v.push_back(data);
                 v.push_back(v_copy[i]);
@@ -42,16 +30,9 @@ class Vetor {
                 v.push_back(v_copy[i]);
             }
         }
-
     }
 
-    void imprimirVetor() {
-        for (int i = 0; i < v.size(); i++) {
-            cout << v[i] << endl;
-        }
-    }
-
-    void removerVector (T data) {
+    void removerValue (T data) {
         vector<T> v_copy;
 
         v_copy = v;
@@ -61,31 +42,53 @@ class Vetor {
         for (int i = 0; i < v_copy.size(); i++) {
             if (v_copy[i] != data) {
                 v.push_back(v_copy[i]);
-            }   
+            }
         }
     }
-
 
     T& operator[] (int index) {
         return v[index];
     }
 
+    void bubble () {
+        for (int i = 0; i < v.size() - 1; i++) {
+            for (int j = 0; j < v.size() - 1 - i; j++) {
+                if (v[j] > v[j + 1]) {
+                    T temp = v[j];
+                    v[j] = v[j + 1];
+                    v[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void imprimir () {
+        for (int i = 0; i < v.size(); i++) {
+            cout << v[i] << endl;
+        }
+    }
+
 
 };
+
 
 int main() {
     Vetor<int> v1;
 
-    v1.inserirVector(20);
-    v1.inserirVector(8);
-    v1.inserirVector(15);
-    v1.removerVector(3);
-    v1.inserirVector(40);
-    v1.inserirVector(21);
-    v1.removerVector(20);  
-    v1.inserirMiddle(100);
-    v1.imprimirVetor();
+    v1.inserir(10);
+    v1.inserir(5);
+    v1.inserir(20);
+    v1.inserir(15);
+    v1.inserir(2);
+    v1.inserir(40);
 
-    cout << "Valor na posição 2: " << v1[2] << endl;
-
+    v1.imprimir();
+    cout << "---------" << endl;
+    v1.bubble();
+    v1.imprimir();
+    cout << "---------" << endl;
+    v1[2] = -6;
+    v1.bubble();
+    v1.inserirMiddle(10000);
+    v1.imprimir();
 }
